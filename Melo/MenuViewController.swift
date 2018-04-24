ad36e8abc706834bbac61c753fbcba9f16304d35
Dipali Bajaj <dipalibajaj9@gmail.com>
2018-04-24 11:36:06 -0400

    Login

--- Melo/MenuViewController.swift
+++ Melo/MenuViewController.swift
@@ -25,21 +25,24 @@ class MenuViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        if let user = Auth.auth().currentUser {
+            self.performSegue(withIdentifier: "toHomeScreen", sender: self)
+        }
+    }
+    
     //Changing the status bar color to white.
     override var preferredStatusBarStyle: UIStatusBarStyle {
         get {
             return .lightContent
         }
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
---
+++ Melo/ProfileViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ProfileViewController.swift
+//  Melo
+//
+//  Created by Dipali Bajaj on 4/20/18.
+//  Copyright © 2018 Dipali Bajaj. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Firebase
+
+class ProfileViewController: UIViewController {
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func handleLogout(_ target: UIBarButtonItem) {
+        try! Auth.auth().signOut()
+        self.dismiss(animated: false, completion: nil)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
